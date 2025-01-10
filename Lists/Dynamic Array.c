d6fdece49628f61784499232a94aca9d8117d626
ahmad <aza29@mail.aub.edu>
2025-01-10 19:23:45 +0200

    Implemented constructors and add method

---
+++ Lists/Dynamic Array.c
@@ -0,0 +1,105 @@
+#include "Dynamic Array.h"
+
+#define DEFAULT_CAPACITY 10
+
+typedef struct DYNAMIC_ARRAY
+{
+    void* data;
+    size_t size;
+    size_t data_size;
+    size_t capacity;
+} dynamic_array;
+
+
+static dynamic_array* allocate_dynamic_array(const size_t data_size, const size_t capacity)
+{
+    dynamic_array* list = malloc(sizeof(dynamic_array));
+
+    if (!list)
+    {
+        fprintf(stderr, "No enough memory for dynamic array!\n");
+        return nullptr;
+    }
+
+    list->capacity = capacity;
+    list->size = 0;
+    list->data_size = data_size;
+    list->data = malloc(data_size * capacity);
+
+    if (!list->data)
+    {
+        fprintf(stderr, "No enough memory for dynamic array data!\n");
+        free(list);
+        return nullptr;
+    }
+
+    return list;
+}
+
+dynamic_array* initialize_dynamic_array_empty(const size_t data_size)
+{
+    return allocate_dynamic_array(data_size, DEFAULT_CAPACITY);
+}
+
+dynamic_array* initialize_dynamic_array_sized(const size_t capacity, const size_t data_size)
+{
+    return allocate_dynamic_array(data_size, capacity);
+}
+
+dynamic_array* initialize_dynamic_array_from(const dynamic_array* list, const size_t data_size)
+{
+    if (!list)
+    {
+        fprintf(stderr, "The source list is null!\n");
+        return nullptr;
+    }
+
+    if (list->data_size != data_size)
+    {
+        fprintf(stderr, "Cannot initialize an array of different data_size\nlist->data_size = %lu != data_size = %lu",
+                list->data_size, data_size);
+        return nullptr;
+    }
+
+    dynamic_array* new_list = allocate_dynamic_array(data_size, list->capacity);
+
+    new_list->size = list->size;
+
+    memcpy(new_list->data, list->data, new_list->size * new_list->data_size);
+
+    return new_list;
+}
+
+int add(dynamic_array* list, const void* data, const size_t data_size)
+{
+    if (list->data_size != data_size)
+    {
+        fprintf(stderr, "Cannot add an element of different data_size\nlist->data_size = %lu != data_size = %lu",
+                list->data_size, data_size);
+        return 0;
+    }
+
+    if (list->size < list->capacity)
+    {
+        void* dest = (char*)list->data + (list->size * data_size);
+        memcpy(dest, data, data_size);
+        list->size++;
+        return 1;
+    }
+
+    list->capacity <<= 1;
+
+    void* dataPtr = realloc(list->data, list->capacity * list->data_size);
+
+    if (!dataPtr)
+    {
+        fprintf(stderr, "No enough memory for dynamic array data resizing!\nOld dynamic data left as is!");
+        return 0;
+    }
+
+    list->data = dataPtr;
+    void* dest = (char*)list->data + (list->size * data_size);
+    memcpy(dest, data, data_size);
+    list->size++;
+    return 1;
+}
--- Lists/Dynamic Array.h
+++ Lists/Dynamic Array.h
@@ -2,35 +2,38 @@
 // Created by ahmad on 1/9/25.
 //
 
-#ifndef DYNAMIC_ARRAY_H
-#define DYNAMIC_ARRAY_H
+#ifndef _DATASTRUCTURES_DYNAMIC_ARRAY_H
+#define _DATASTRUCTURES_DYNAMIC_ARRAY_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef struct DYNAMIC_ARRAY dynamic_array;
 
 
-dynamic_array* initialize_dynamic_array_empty(size_t data_size);
-dynamic_array* initialize_dynamic_array_sized(int size, size_t data_size);
-dynamic_array* initialize_dynamic_array_from(dynamic_array* list, size_t data_size);
-int add(dynamic_array* list, void* data);
-int insert(dynamic_array* list, int index, void* data);
+dynamic_array* initialize_dynamic_array_empty(const size_t data_size);
+dynamic_array* initialize_dynamic_array_sized(const size_t capacity, const size_t data_size);
+dynamic_array* initialize_dynamic_array_from(const dynamic_array* list, const size_t data_size);
+int add(dynamic_array* list, const void* data, const size_t data_size);
+int insert(dynamic_array* list, const size_t index, const void* data, const size_t data_size);
 int add_all(dynamic_array* list, dynamic_array* other_list);
 void clear(dynamic_array* list);
 dynamic_array* clone(dynamic_array* list);
-int contains(dynamic_array* list, void* data);
-void ensure_capacity(dynamic_array* list, int capacity);
-void* get(dynamic_array* list, int i);
-int index_of(dynamic_array* list, void* data);
+int contains(dynamic_array* list, const void* data, const size_t data_size);
+void ensure_capacity(dynamic_array* list, size_t capacity);
+void* get(dynamic_array* list, const size_t index);
+int index_of(dynamic_array* list, const void* data, const size_t data_size);
 int is_empty(dynamic_array* list);
-int last_index_of(dynamic_array* list, void* data);
-void* remove_at(dynamic_array* list, int index);
-int remove_element(dynamic_array* list, void* data);
+int last_index_of(dynamic_array* list, const void* data, const size_t data_size);
+void* remove_at(dynamic_array* list, const size_t index);
+int remove_element(dynamic_array* list, const void* data, const size_t data_size);
 int remove_all(dynamic_array* list, dynamic_array* other_list);
-void remove_range(dynamic_array* list, int start, int end);
+void remove_range(dynamic_array* list, const size_t start, const size_t end);
 size_t size(dynamic_array* list);
 void sort(dynamic_array* list, int (*compar)(const void*, const void*));
-dynamic_array* get_sub_list(dynamic_array* list, int start, int end);
+dynamic_array* get_sub_list(dynamic_array* list, const size_t start, const size_t end);
 void trim_to_size(dynamic_array* list);
 
-#endif //DYNAMIC_ARRAY_H
+#endif //_DATASTRUCTURES_DYNAMIC_ARRAY_H
