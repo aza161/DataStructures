8e719b108e15e3c7f31cdbda9f310c94726c1401
ahmad <aza29@mail.aub.edu>
2025-01-10 21:34:49 +0200

    Implemented grow method and insert

--- Lists/Dynamic Array.c
+++ Lists/Dynamic Array.c
@@ -70,23 +70,8 @@ dynamic_array* initialize_dynamic_array_from(const dynamic_array* list, const si
     return new_list;
 }
 
-int add(dynamic_array* list, const void* data, const size_t data_size)
+static int grow_dynamic_list(dynamic_array* list)
 {
-    if (list->data_size != data_size)
-    {
-        fprintf(stderr, "Cannot add an element of different data_size\nlist->data_size = %lu != data_size = %lu",
-                list->data_size, data_size);
-        return 0;
-    }
-
-    if (list->size < list->capacity)
-    {
-        void* dest = (char*)list->data + (list->size * data_size);
-        memcpy(dest, data, data_size);
-        list->size++;
-        return 1;
-    }
-
     list->capacity <<= 1;
 
     void* dataPtr = realloc(list->data, list->capacity * list->data_size);
@@ -98,8 +83,54 @@ int add(dynamic_array* list, const void* data, const size_t data_size)
     }
 
     list->data = dataPtr;
-    void* dest = (char*)list->data + (list->size * data_size);
-    memcpy(dest, data, data_size);
-    list->size++;
     return 1;
+}
+
+int add(dynamic_array* list, const void* data, const size_t data_size)
+{
+    if (list->data_size != data_size)
+    {
+        fprintf(stderr, "Cannot add an element of different data_size\nlist->data_size = %lu != data_size = %lu",
+                list->data_size, data_size);
+        return 0;
+    }
+
+    // A short circuit occurs and no resizing occurs if size < capacity
+    if (list->size < list->capacity || grow_dynamic_list(list))
+    {
+        void* dest = (char*)list->data + (list->size * data_size);
+        memcpy(dest, data, data_size);
+        list->size++;
+        return 1;
+    }
+
+    return 0;
+}
+
+int insert(dynamic_array* list, const size_t index, const void* data, const size_t data_size)
+{
+    if (list->data_size != data_size)
+    {
+        fprintf(stderr, "Cannot add an element of different data_size\nlist->data_size = %lu != data_size = %lu",
+                list->data_size, data_size);
+        return 0;
+    }
+
+    // A short circuit occurs and no resizing occurs if size < capacity
+    if (list->size < list->capacity || grow_dynamic_list(list))
+    {
+        if (index < list->size)
+        {
+            void* dest = (char*)list->data + ((index + 1) * data_size);
+            void* src = (char*)list->data + (index * data_size);
+            size_t size = (list->size - index) * data_size;
+            memmove(dest, src, size);
+        }
+        void* dest = (char*)list->data + (index * data_size);
+        memcpy(dest, data, data_size);
+        list->size++;
+        return 1;
+    }
+
+    return 0;
 }
