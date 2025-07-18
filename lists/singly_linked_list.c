6810c78cdc6741598d33121e85bc8ed657003c19
ahmad <aza29@mail.aub.edu>
2025-07-18 17:01:28 +0300

    feat(slist): Implement get functions, simplify logic for `slist_initialize_from`, and add `s_node_get_kth`.

--- lists/singly_linked_list.c
+++ lists/singly_linked_list.c
@@ -15,7 +15,6 @@
 
 #include "singly_linked_list.h"
 
-#include <stdlib.h>
 
 /* Singly linked node */
 typedef struct s_node s_node;
@@ -45,6 +44,7 @@ typedef struct singly_linked_list
 static s_node* s_node_initialize(const void* data, const size_t data_size);
 static bool s_node_destroy(s_node* node);
 static bool s_node_has_next(const s_node* node);
+static s_node* s_node_get_kth(s_node* node, const size_t k);
 
 
 static s_node* s_node_initialize(const void* data, const size_t data_size)
@@ -83,6 +83,26 @@ static bool s_node_has_next(const s_node* node)
     return node->next != nullptr;
 }
 
+static s_node* s_node_get_kth(s_node* node, const size_t k)
+{
+    if (!node)
+    {
+        return nullptr;
+    }
+
+    size_t i = 0;
+    while (i != k)
+    {
+        node = node->next;
+        if (!node)
+        {
+            return nullptr;
+        }
+        i++;
+    }
+    return node;
+}
+
 singly_linked_list* slist_initialize(const size_t data_size)
 {
     singly_linked_list* list = calloc(1, sizeof(singly_linked_list));
@@ -99,7 +119,7 @@ singly_linked_list* slist_initialize(const size_t data_size)
 
 singly_linked_list* slist_initialize_from(const singly_linked_list* list, const size_t data_size)
 {
-    if (!list || data_size != list->data_size || !list->head)
+    if ((!list) || (data_size != list->data_size) || (!list->head))
     {
         return nullptr;
     }
@@ -124,25 +144,24 @@ singly_linked_list* slist_initialize_from(const singly_linked_list* list, const 
 
     memcpy(new_list_current->data, list_current->data, data_size);
     new_list->tail = new_list_current;
-    new_list->size++;
 
     while (s_node_has_next(list_current))
     {
-        list_current = list_current->next;
-
-        new_list_current->next = s_node_initialize(nullptr, data_size);
+        new_list_current->next = s_node_initialize(list_current->next->data, data_size);
         if (!new_list_current->next)
         {
             slist_destroy(new_list);
             return nullptr;
         }
+
         new_list_current = new_list_current->next;
 
-        new_list->tail = new_list_current;
-
-        memcpy(new_list_current->data, list_current->data, data_size);
-        new_list->size++;
+        list_current = list_current->next;
     }
+
+    new_list->tail = new_list_current;
+    new_list->size = list->size;
+
     return new_list;
 }
 
@@ -163,4 +182,104 @@ bool slist_destroy(singly_linked_list* list)
 
     free(list);
     return true;
+}
+
+
+void* slist_get_first(const singly_linked_list* list)
+{
+    if ((!list) || (!list->size) || (!list->head))
+    {
+        return nullptr;
+    }
+
+    return list->head->data;
+}
+
+void* slist_get_last(const singly_linked_list* list)
+{
+    if ((!list) || (!list->size) || (!list->tail))
+    {
+        return nullptr;
+    }
+
+    return list->tail->data;
+}
+
+void* slist_get_at(const singly_linked_list* list, const size_t index)
+{
+    if ((!list) || (index >= list->size) || (!list->head))
+    {
+        return nullptr;
+    }
+
+    size_t i = 0;
+    const s_node* current = list->head;
+
+    while (i != index)
+    {
+        current = current->next;
+        i++;
+    }
+
+    return current->data;
+}
+
+singly_linked_list* slist_get_sub_list(const singly_linked_list* list, const size_t start, const size_t end)
+{
+    if ((!list) || (start >= list->size) || (end > list->size) || (start >= end) || (!list->head))
+    {
+        return nullptr;
+    }
+
+    const size_t data_size = list->data_size;
+
+    singly_linked_list* sub_list = slist_initialize(data_size);
+
+    if (!sub_list)
+    {
+        return nullptr;
+    }
+
+    sub_list->head = s_node_initialize(list->head->data, data_size);
+
+    if (!sub_list->head)
+    {
+        slist_destroy(sub_list);
+        return nullptr;
+    }
+
+    sub_list->tail = sub_list->head;
+    sub_list->size++;
+
+    s_node* sub_list_current = sub_list->head;
+    const s_node* list_current = s_node_get_kth(list->head, start);
+
+    if (!list_current)
+    {
+        slist_destroy(sub_list);
+        return nullptr;
+    }
+
+    const size_t total_elements = end - start;
+
+    while (sub_list->size < total_elements)
+    {
+        sub_list_current->next = s_node_initialize(list_current->next->data, data_size);
+
+        if (!sub_list_current->next)
+        {
+            slist_destroy(sub_list);
+            return nullptr;
+        }
+
+        sub_list_current = sub_list_current->next;
+
+        list_current = list_current->next;
+
+        sub_list->size++;
+    }
+
+    sub_list->tail = sub_list_current;
+
+    return sub_list;
 }
